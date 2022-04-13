5247e7d534d9019a055494af4c0c8a723235e8df
Fran Fernández <frankzzcsgo@gmail.com>
2022-04-13 14:09:20 +0200

    arreglado botones del header centrados

--- adoptamefront/header.php
+++ adoptamefront/header.php
@@ -1,14 +1,14 @@
 <div class="title-block" style="float: left; display: flex; background-color: #F1C232; height: calc(100% - 80%) !important;">
-    <div class="column-left-right" style="float: left; width: 25%">
-        <img src="images/redes.png" alt="redes" style="width: 100%; max-width: 40%;">
+    <div class="column-left-right" style="float: left; width: 25%; position: relative;">
+        <img src="images/redes.png" alt="redes" style="max-height: 47%;max-width: 40%;position: absolute;top: 0;bottom: 0;left: 0;right: 0;margin: auto;">
     </div>
 
     <div class="column-center" style="float: left; width: 50%;">
         <img src="images/banner.png" alt="banner" style="width: 100%; padding-top: 10px; padding-bottom: 10px;">
     </div>
 
-    <div class="column-left-right" style="float: left; width: 25%;">
-        <img src="images/perfil.png" alt="redes" style="width: 100%; max-width: 15%;">
+    <div class="column-left-right" style="float: left; width: 25%; position: relative;">
+        <img src="images/perfil.png" alt="redes" style="max-height: 25%;max-width: 19%;position: absolute;top: 0;bottom: 0;left: 0;right: 0;margin: auto;">
     </div>
 </div>
 
