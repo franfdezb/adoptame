bc219b520d09f0dab99f76617556894095873235
Fran Fernández <frankzzcsgo@gmail.com>
2022-04-13 22:08:44 +0200

    añadido dropdown login header y responsive header y algunas partes más

--- adoptamefront/header.php
+++ adoptamefront/header.php
@@ -4,11 +4,11 @@
     </div>
 
     <div class="column-center" style="float: left; width: 50%;">
-        <img src="images/banner.png" alt="banner" style="width: 65%; padding-top: 10px; padding-bottom: 10px;">
+        <img src="images/banner.png" id="fotobanner" alt="banner" style="width: 65%; padding-top: 10px; padding-bottom: 10px;">
     </div>
 
     <div class="dropdown" style="float: left; width: 25%; position: relative;">
-    <img src="images/perfil.png" onclick="myFunction()" class="dropbtn" alt="redes" style="max-height: 50%;max-width: 33%;position: absolute;top: 0;bottom: 0;left: 0;right: 0;margin: auto;">
+    <img src="images/perfil.png" id="fotoperfil" onclick="myFunction()" class="dropbtn" alt="redes" style="max-height: 50%;max-width: 33%;position: absolute;top: 0;bottom: 0;left: 0;right: 0;margin: auto;">
         <div id="myDropdown" class="dropdown-content">
             <a href="http://localhost:3000/adoptamefront/login.php" id="optiononeprofile">Inicia sesión</a>
             <a href="#signup" id="optiontwoprofile">Regístrate</a>
@@ -17,10 +17,11 @@
 </div>
 
     <nav class="navbar navbar-expand-sm bg-light navbar-light" style="float: left; width: 100%; margin-bottom: 20px;">
-        <a href="http://localhost:3000/adoptamefront/index.php" class="button" style="width: 25%; text-align: center; color: black !important; font-weight: bold; font-family: Open Sans;">INICIO</a>
-        <a href="/about" class="button" style="width: 25%; text-align: center; color: black !important; font-weight: bold; font-family: Open Sans;">PROTECTORAS</a>
-        <a href="#" class="button" style="width: 25%; text-align: center; color: black !important; font-weight: bold; font-family: Open Sans;">DA EN ADOPCIÓN</a>
-        <a href="#" class="button" style="width: 25%; text-align: center; color: black !important; font-weight: bold; font-family: Open Sans;">¿QUIÉNES SOMOS?</a>
+        <a href="http://localhost:3000/adoptamefront/index.php" class="button" id="button" style="width: 25%; text-align: center; color: black !important; font-weight: bold; font-family: Open Sans;">INICIO</a>
+        <a href="/about" class="button" id="button" style="width: 25%; text-align: center; color: black !important; font-weight: bold; font-family: Open Sans;">PROTECTORAS</a>
+        <a href="#" class="button" id="button" style="width: 25%; text-align: center; color: black !important; font-weight: bold; font-family: Open Sans;">DA EN ADOPCIÓN</a>
+        <a href="#" class="button" id="button" style="width: 25%; text-align: center; color: black !important; font-weight: bold; font-family: Open Sans;">¿QUIÉNES SOMOS?</a>
+        <button href="#" class="buttonmovil" id="buttonmenumovil" style="width: 25%; text-align: center; color: black !important; font-weight: bold; font-family: Open Sans; display: none;">MENÚ</button>
     </nav>
   
 </div>
